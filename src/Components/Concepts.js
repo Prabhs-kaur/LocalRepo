8e772c289fd952fb24e023233f52ac2bcc8019f4
Prabhs-Kaur <prabhjotkaur1243@gmail.com>
2023-11-16 14:20:06 -0800

    Add Initial Files

---
+++ src/Components/Concepts.js
@@ -0,0 +1,15 @@
+function Concepts(props){
+return (
+
+    <ul id="concepts">
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  </ul>
+
+);
+}
+
+export default Concepts;
